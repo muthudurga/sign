43a2954fdd780a6b2f0f95eb4f7304f42c4cfb6e
muthudurga <37535321+muthudurga@users.noreply.github.com>
2019-07-23 15:33:38 +0530

    Delete sign.py

--- sign.py
+++ sign.py
@@ -1,16 +1,14 @@
 def sign():
-    a=1
-    while(a>0):
-        n=input( )
-        try:
-            x=int(n)
-            if x>0:
-                print("Positive")
-            elif x<0:
-                print("Negative")
-            else:
-                print("Zero")
-        except:
-            print(n+" is not a number")
+    n=input( )
+    try:
+        x=int(n)
+        if x>0:
+            print("Positive")
+        elif x<0:
+            print("Negative")
+        else:
+            print("Zero")
+    except:
+        print(n+" is not a number")
 sign()
     
