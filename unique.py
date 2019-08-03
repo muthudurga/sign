f7cc1e81d0d3d688d284fff77f3146676a00e555
muthudurga <37535321+muthudurga@users.noreply.github.com>
2019-08-03 23:55:21 +0530

    Add files via upload

---
+++ unique.py
@@ -0,0 +1,10 @@
+N=int(input())
+z=[int(z) for z in input().split()]
+a=[]
+for i in range(N):
+    k=i+1
+    for j in range(k,N):
+        if z[i]==z[j]:
+            if z[i] not in a:
+                a.append(z[i])
+print(*a)
