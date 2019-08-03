44d0feee7ce8b1679f1ee73649bf19fa7c170f39
muthudurga <37535321+muthudurga@users.noreply.github.com>
2019-08-03 22:25:17 +0530

    Add files via upload

---
+++ odd.py
@@ -0,0 +1,11 @@
+N,Q=[int(N) for N in input().split()]
+z=[]
+for i in range(N+1,Q):
+    for x in range(N+1,Q):
+        if i%x==0:
+            break
+        else:
+            z.append(i)
+            break
+print(*z)
+            
